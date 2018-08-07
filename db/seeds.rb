13022378aa56c115dae22cd52f560af82288e45a
Caio Andrade <caioertai@gmail.com>
2018-08-07 18:04:20 -0300

    Created the controller and the seed
    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all pets...'
+Pet.destroy_all
+
+puts 'Creating pets...'
+10.times do
+  Pet.create!(
+    name: Faker::Pokemon.name,
+    address: Faker::Pokemon.location,
+    found_on: (Date.today - rand(10)),
+    species: %w[cat dog parrot dinossaur].sample
+  )
+end
