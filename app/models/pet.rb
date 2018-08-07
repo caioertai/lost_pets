8e9003981742ce929b82ddf083e6f00ef22b3dea
Caio Andrade <caioertai@gmail.com>
2018-08-07 18:20:25 -0300

    Created unit tests for #found_days_ago

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   validates :name, :address, :found_on, :species, presence: true
+
+  def found_days_ago
+    days_since_found = (Date.today - found_on).to_i
+    "found #{days_since_found} days ago"
+  end
 end
--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#found_days_ago should return a string' do
+    pet_found_6_days_ago = pets(:pet_found_6_days_ago)
+    assert_equal 'found 6 days ago', pet_found_6_days_ago.found_days_ago
+  end
+
+  test '#found_days_ago should return the correct text' do
+    pet_found_6_days_ago = pets(:pet_found_6_days_ago)
+    assert_instance_of String, pet_found_6_days_ago.found_days_ago
+  end
 end
