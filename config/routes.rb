94bf3662450bdc4fee31ae40a48737122ed51766
Caio Andrade <caioertai@gmail.com>
2018-08-07 18:36:58 -0300

    Created the show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+
+  # Create
+  get 'pets/new', to: 'pets#new'
+  post 'pets', to: 'pets#create'
+
+  # Read
   get 'pets', to: 'pets#index'
+  get 'pets/:id', to: 'pets#show', as: :pet
+
+  # Update
+  get 'pets/:id/edit', to: 'pets#edit'
+  patch 'pets/:id', to: 'pets#update'
+
+  # Delete/Destroy
+  delete 'pets/:id', to: 'pets#destroy'
 
   root to: 'pets#index'
 end
